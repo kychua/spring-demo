effa12c242ffe9e76792d59ff5ea931c5e23e4bc
Ka Yi <chua.kayi@yahoo.com.sg>
2018-11-16 16:49:06 +0800

    Add Spring to MySQL
    Note that repository and other files must be in subpackage of `hello`,
    otherwise they are not included in SpringBootApplication's component
    scan. If we want to include other packages, we will need to add them
    manually to @SpringBootApplication using @ComponentScan.
    
    https://dzone.com/articles/spring-spring-boot-and-component-scan

---
+++ src/main/java/hello/ebookshop/Book.java
@@ -0,0 +1,64 @@
+package hello.ebookshop;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name="books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String author;
+    private float price;
+    private int qty;
+
+    public Book() {
+
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+}
