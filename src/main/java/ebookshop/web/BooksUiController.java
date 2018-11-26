a264db366f40be036e3567691c48e9bf4526e7ad
Ka Yi <chua.kayi@yahoo.com.sg>
2018-11-26 17:59:30 +0800

    Add form submission example

--- src/main/java/ebookshop/web/BooksUiController.java
+++ src/main/java/ebookshop/web/BooksUiController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,6 +37,11 @@ public class BooksUiController {
 		return "books";
 	}
 
+	@PostMapping
+	public String addBook(@ModelAttribute Book book) {
+		bookService.createBook(book);
+		return "books";
+	}
 
 }
 
